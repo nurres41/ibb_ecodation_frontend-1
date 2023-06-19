063e719647346e799f70687ab556e18c7aa241c5
Nuri Onur <nurionurkurtulus@gmail.com>
2023-06-19 21:13:23 +0300

    Ekleme yapıldı

--- template.js
+++ template.js
@@ -1,2 +1,20 @@
-// alert("Deneme")
-// window.alert("Deneme")
+
+// Dark mode
+let darkMode = () => {
+    window.document.classList.toggle("dark_mode")
+}
+
+$(document).ready(function(){
+    const searchApi = ["Kocaeli","Adana","Antep","Bursa","Edirne"];
+    $("tags").autocomplete({
+        source:searchApi
+    })    
+});
+
+//footer
+
+let newDate=()=>{
+    document.getElementById("date_id").innerHTML = new Date().getFullYear()
+}
+
+newDate()
